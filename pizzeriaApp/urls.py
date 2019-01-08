8f378d4b717c70be58ad23829882b4c29b158589
Yonder <yonderrosales96@gmail.com>
2019-01-08 11:46:31 -0400

    creado index de cliente,form base para pizzas y algunos archivos base(crud) para pizza que deben ser editados

--- pizzeriaApp/urls.py
+++ pizzeriaApp/urls.py
@@ -3,6 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cliente',views.create,name = 'crearcliente'),
-    path('<int:id>/cliente',views.show,name = 'mostrarcliente')
+    path('<int:id>/cliente',views.show,name = 'mostrarcliente'),
+    path('cliente/index',views.index,name= 'indexcliente'),
+    path('pizza',views.createpizza, name = 'createpizza'),
+    path('<int:id>/pizza',views.showpizza)
 
 ]
